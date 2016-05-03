72bd8af8c821ce95a33f0fa20d580655bf299253
aptbs85 <jzamparas@gmail.com>
2016-05-03 18:55:40 +0000

    Course 5.3a

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
   
   resources :orders, only: [:index, :show, :create, :destroy]
+  
+  post 'static_pages/thank_you'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

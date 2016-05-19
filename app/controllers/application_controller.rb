e4e0cb3374617cc20b507c782db2766ef23b8dc3
aptbs85 <jzamparas@gmail.com>
2016-05-19 22:04:56 +0000

    Course 5.5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
   UserMailer.contact_form(@email, @name, @message).deliver_now
   end
   
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
+  
 end
 

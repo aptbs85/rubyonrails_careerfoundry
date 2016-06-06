52fc4219c25593881faaa7d4d9cac39448bf6766
aptbs85 <jzamparas@gmail.com>
2016-06-06 20:54:38 +0000

    adminrole

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,18 +4,15 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.save
+    redirect_to product_path(@product)
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
   
   
@@ -24,7 +21,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
-  
-  
   
 end

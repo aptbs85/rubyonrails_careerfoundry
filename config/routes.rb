88c24f72433dcfe61356a1b18f1af0c7d6815b06
aptbs85 <jzamparas@gmail.com>
2016-07-22 15:36:06 +0000

    newlayout

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'products#index'
   
   resources :orders, only: [:index, :show, :create, :destroy]
 
